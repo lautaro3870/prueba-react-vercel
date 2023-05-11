845719c95207d059b7b1c13c5edbcd26e100dfa3
lautaro3870 <lautarodisbro@gmail.com>
2023-05-11 15:11:21 +0200

    Listo

---
+++ src/Listado.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import './style.css'
+
+const getLocalItems = () => {
+  let list = localStorage.getItem("items");
+  if (list === null) {
+    return [];
+  }
+  console.log(list);
+  if (list) {
+    return JSON.parse(localStorage.getItem("items"));
+  }
+};
+
+export default function Listado() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState(getLocalItems());
+
+  function addItem() {
+    console.log(newItem);
+
+    if (!newItem) {
+      alert("Ingrese un item");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
+  }
+
+  //agregar los items al localstorage
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addItem();
+    }
+  };
+
+  return (
+    <div>
+      <br />
+      <h1>General</h1>
+      <input
+        onKeyDown={handleKeyDown}
+        type="text"
+        placeholder="Ingrese tarea"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.value}{" "}
+              <button
+                className="delete-button"
+                onClick={() => deleteItem(item.id)}
+              >
+                ❌
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
